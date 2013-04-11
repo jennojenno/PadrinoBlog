65f753c5225ec765b68837275f4f53a2a356c30b
Jen Lim <jennolim@gmail.com>
2013-04-11 15:43:05 -0400

    Not sure

--- models/post.rb
+++ models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
 	belongs_to :author 
 	has_many :comments
 	has_many :post_tags
-	has_many :tags, :through => :post_tags
+	has_many :tags, :through => :post_tags #, :order => 'created_at desc'
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,10 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/../test_config.rb')
 
 class CommentTest < Test::Unit::TestCase
-  context "Comment Model" do
-    should 'construct new instance' do
-      @comment = Comment.new
-      assert_not_nil @comment
-    end
+	context "Comment Model" do
+		setup do
+			@post = Post.new(:title =>"Blogpost" :body => "Body of blog post" :author => "Jen")
+			@tag = Tag.new(:name ="rubytag")
+			@post_tag = PostTag.new(:post => @post, :tag => @tag)
+			@post.post_tags << @post_tag 
+			@post.save! 
+		end 
+
+
+		should 'construct new instance' do
+      assert @post.respond_to?(:post_tags) #this is plural bc post.rb has many post_tags
+      assert_equals 1, @post_tag 
   end
 end
